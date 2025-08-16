7b56062b84843615272820117663f62e3073a021
LeonRodrigues55 <223340341+LeonRodrigues55@users.noreply.github.com>
2025-08-16 14:34:25 +0000

    Desafio mestre concluido com varios loops infinito deu para arrumar tudo mas com muito trabalho e tive que apelar para IA me dizer qual parte do codigo estava errada mas aprendi e deu certo!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,6 +36,84 @@ int main() {
         tabuleiro[l4 + i][c4 - i] = 3;
     }
 
+    //Habilidades
+    int tamHab = 5;
+    int cone[5][5];
+    int cruz[5][5];
+    int octaedro[5][5];
+
+    //Preencher Cone
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            if (j >= (tamHab / 2 - i) && j <= (tamHab / 2 + i))
+                cone[i][j] = 1;
+            else
+                cone[i][j] = 0;
+        }
+    }
+
+    //Preencher Cruz
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            if (i == tamHab / 2 || j == tamHab / 2)
+                cruz[i][j] = 1;
+            else
+                cruz[i][j] = 0;
+        }
+    }
+
+    //Preencher Octaedro
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            if (abs(i - tamHab / 2) + abs(j - tamHab / 2) <= tamHab / 2)
+                octaedro[i][j] = 1;
+            else
+                octaedro[i][j] = 0;
+        }
+    }
+
+    // Origem das habilidades no tabuleiro
+    int origemConeL = 4, origemConeC = 4;
+    int origemCruzL = 1, origemCruzC = 8;
+    int origemOctL = 7, origemOctC = 5;
+
+    // Função genérica para aplicar habilidade
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            int li = origemConeL + i - tamHab / 2;
+            int co = origemConeC + j - tamHab / 2;
+            if (li >= 0 && li < tamanho && co >= 0 && co < tamanho) {
+                if (cone[i][j] == 1 && tabuleiro[li][co] == 0) {
+                    tabuleiro[li][co] = 5;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            int li = origemCruzL + i - tamHab / 2;
+            int co = origemCruzC + j - tamHab / 2;
+            if (li >= 0 && li < tamanho && co >= 0 && co < tamanho) {
+                if (cruz[i][j] == 1 && tabuleiro[li][co] == 0) {
+                    tabuleiro[li][co] = 5;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < tamHab; i++) {
+        for (int j = 0; j < tamHab; j++) {
+            int li = origemOctL + i - tamHab / 2;
+            int co = origemOctC + j - tamHab / 2;
+            if (li >= 0 && li < tamanho && co >= 0 && co < tamanho) {
+                if (octaedro[i][j] == 1 && tabuleiro[li][co] == 0) {
+                    tabuleiro[li][co] = 5;
+                }
+            }
+        }
+    }
+
     //Tabuleiro na tela
     printf("   TABULEIRO BATALHA NAVAL\n\n   ");
     for (int letra = 0; letra < tamanho; letra++) {
@@ -46,7 +124,12 @@ int main() {
     for (int i = 0; i < tamanho; i++) {
         printf("%2d ", i + 1);
         for (int j = 0; j < tamanho; j++) {
-            printf(" %d", tabuleiro[i][j]);
+            if (tabuleiro[i][j] == 0)
+                printf(" ~"); // Água
+            else if (tabuleiro[i][j] == 3)
+                printf(" N"); // Navio
+            else if (tabuleiro[i][j] == 5)
+                printf(" *"); // Área habilidade
         }
         printf("\n");
     }
